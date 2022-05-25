36958c23bba44f2fe5f0ad49907abecf8e972329
greydevv <greysonmurray.dev@gmail.com>
2022-05-25 19:51:20 -0400

    impl home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,7 @@
-import { Outlet } from 'react-router-dom';
-import homeBanner from 'assets/images/home_banner.png';
+import ShowcaseCard from 'components/ShowcaseCard';
+import homeBanner from 'assets/images/home/home_banner.png';
+import huntsThumb from 'assets/images/home/hunts_thumb.jpg';
+import trainingThumb from 'assets/images/home/training_thumb.jpg';
 import 'pages/Home.scss';
 
 export default function Home() {
@@ -12,6 +14,29 @@ export default function Home() {
                     <div className='empty_border' />
                     <h6>EST 2021</h6>
                 </div>
+            </div>
+            <div className='showcase_cards'>
+                <ShowcaseCard 
+                    img={ huntsThumb } 
+                    headText='hunts' 
+                    text='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+                    moreText='learn more'
+                />
+                <ShowcaseCard 
+                    img={ trainingThumb } 
+                    headText='training' 
+                    text='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+                    moreText='learn more'
+                    reverse
+                />
+                {/*
+                <ShowcaseCard 
+                    img={ huntsThumb } 
+                    headText='studio' 
+                    text='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+                    moreText='learn more'
+                />
+                */}
             </div>
         </div>
     );
