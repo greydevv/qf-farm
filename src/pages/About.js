4ccddf2c74adddeed69e81b4ecbf15c5ac7efbea
greydevv <greysonmurray.dev@gmail.com>
2022-06-05 08:24:42 -0400

    make /about responsive for phones

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,31 +1,33 @@
 import ShowcaseCard from 'components/ShowcaseCard';
+import cristin from 'assets/images/about/cristin.jpg';
+import brandt from 'assets/images/about/brandt.jpg';
 import about from 'assets/images/about/about.jpg';
 
-function Profile({ name, bio }) {
+function Profile({ name, bio, img }) {
     return (
-        <div className='flex flex-row flex-col gap-10'>
-            <div className='h-[200px] aspect-square rounded overflow-clip'>
-                <img src={ about } className='h-full object-cover' />
+        <div className='flex desktop:flex-row flex-col desktop:gap-10 gap-6 desktop:px-0 px-6'>
+            <div className='desktop:h-[200px] desktop:mr-0 mr-auto desktop:aspect-square aspect-[4/3] rounded overflow-clip'>
+                <img src={ img } className='w-full h-full object-cover' />
             </div>
-            <div>
-                <h1 className='font-1 text-3xl mb-4 font-normal text-qf-black'>{ name }</h1>
-                <p className='text-qf-grey w-4/5 leading-8'>{ bio }</p>
+            <div className='flex-1 desktop:w-4/5'>
+                <h1 className='tracking-widest font-1 text-3xl mb-4 font-normal text-qf-black'>{ name }</h1>
+                <p className='text-qf-grey leading-8 font-light'>{ bio }</p>
             </div>
         </div>
     );
 }
 
-function DogProfile({ name, bio }) {
+function DogProfile({ name, bio, img }) {
     return (
         <div className='flex flex-col justify-center'>
             <div className='h-[100px] aspect-square mx-auto rounded-full overflow-clip'>
-                <img src={ about } className='h-full object-cover' />
+                <img src={ img } className='h-full object-cover' />
             </div>
             <div className='w-full text-center w-4/5 max-w-sm mx-auto'>
                 <h1 className='font-1 text-3xl my-4 font-normal text-qf-black'>
                     { name }
                 </h1>
-                <p className='text-qf-grey leading-8'>
+                <p className='text-qf-grey leading-8 font-light'>
                     { bio }
                 </p>
             </div>
@@ -35,13 +37,13 @@ function DogProfile({ name, bio }) {
 
 export default function About() {
     return (
-        <div className='about desktop:px-40 desktop:my-20 phone:px-10 phone:my-5 flex flex-col gap-20'>
-            <div className='flex flex-row gap-10'>
-                <div className='w-1/2'>
-                    <h1 className='w-full font-1 text-5xl mb-4 font-normal text-qf-black'>
+        <div className='desktop:px-40 desktop:my-20 mb-5 flex flex-col gap-20'>
+            <div className='flex desktop:flex-row flex-col desktop:gap-10 gap-6'>
+                <div className='desktop:w-1/2 desktop:px-0 px-6 text-left'>
+                    <h1 className='w-full font-1 desktop:text-5xl text-3xl desktop:mb-4 mb-2 font-normal text-qf-black'>
                         OUR STORY
                     </h1>
-                    <p className='w-full text-qf-grey leading-8'>
+                    <p className='desktop:w-9/10 text-qf-grey leading-8 font-light'>
                         Lorem ipsum dolor sit amet, consectetur adipiscing
                         elit, sed do eiusmod tempor incididunt ut labore et dolore magna
                         aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
@@ -51,34 +53,40 @@ export default function About() {
                         qui officia deserunt mollit anim id est laborum.
                     </p>
                 </div>
-                <div className='h-full w-1/2 overflow-clip rounded lg:order-last order-first'>
+                <div className='mx-auto h-full w-full overflow-clip desktop:rounded desktop:order-last order-first'>
                     <img src={ about } />
                 </div>
             </div>
             <Profile 
                 name='CRISTIN Q. MURRAY'
                 bio='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
+                img={ cristin }
             />
             <Profile
                 name='BRANDT F. FINK'
                 bio='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
+                img={ brandt }
             />
-            <div className='grid grid-cols-2 grid-rows-2 gap-y-8'>
+            <div className='grid desktop:grid-cols-2 desktop:grid-rows-2 grid-rows-4 gap-y-8'>
                 <DogProfile 
                     name='MOLSON' 
                     bio='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.' 
+                    img={ about }
                 />
                 <DogProfile 
                     name='REMMEL' 
                     bio='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.' 
+                    img={ about }
                 />
                 <DogProfile 
                     name='CAMO' 
                     bio='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.' 
+                    img={ about }
                 />
                 <DogProfile 
                     name='MODELO' 
                     bio='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.' 
+                    img={ about }
                 />
             </div>
         </div>
