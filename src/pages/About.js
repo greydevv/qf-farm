30459369c483743350d65a6656a49e9ee477cb88
greydevv <greysonmurray.dev@gmail.com>
2022-06-05 12:09:36 -0400

    responsivity and refactoring

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,6 +3,14 @@ import cristin from 'assets/images/about/cristin.jpg';
 import brandt from 'assets/images/about/brandt.jpg';
 import about from 'assets/images/about/about.jpg';
 
+const bodyCls = (extra) => {
+    const base = 'text-qf-grey leading-8 font-light'
+    if (extra) {
+        return `${base} ${extra}`
+    }
+    return base
+}
+
 function Profile({ name, bio, img }) {
     return (
         <div className='flex desktop:flex-row flex-col desktop:gap-10 gap-6 desktop:px-0 px-6'>
@@ -10,8 +18,8 @@ function Profile({ name, bio, img }) {
                 <img src={ img } className='w-full h-full object-cover' />
             </div>
             <div className='flex-1 desktop:w-4/5'>
-                <h1 className='tracking-widest font-1 text-3xl mb-4 font-normal text-qf-black'>{ name }</h1>
-                <p className='text-qf-grey leading-8 font-light'>{ bio }</p>
+                <h1 className='tracking-widest font-1 desktop:text-3xl text-2xl mb-4 font-normal text-qf-black'>{ name }</h1>
+                <p className={ bodyCls() }>{ bio }</p>
             </div>
         </div>
     );
@@ -19,15 +27,17 @@ function Profile({ name, bio, img }) {
 
 function DogProfile({ name, bio, img }) {
     return (
-        <div className='flex flex-col justify-center'>
-            <div className='h-[100px] aspect-square mx-auto rounded-full overflow-clip'>
-                <img src={ img } className='h-full object-cover' />
-            </div>
-            <div className='w-full text-center w-4/5 max-w-sm mx-auto'>
-                <h1 className='font-1 text-3xl my-4 font-normal text-qf-black'>
+        <div className='flex flex-col justify-center desktop:gap-0 gap-6'>
+            <div className='flex desktop:flex-col flex-row gap-x-6'>
+                <div className='h-[100px] aspect-square desktop:mx-auto rounded-full overflow-clip'>
+                    <img src={ img } className='h-full object-cover' />
+                </div>
+                <h1 className='desktop:text-center desktop:my-4 my-auto font-1 desktop:text-3xl text-2xl tracking-widest font-normal text-qf-black'>
                     { name }
                 </h1>
-                <p className='text-qf-grey leading-8 font-light'>
+            </div>
+            <div className=''>
+                <p className={ bodyCls('desktop:text-center') }>
                     { bio }
                 </p>
             </div>
@@ -43,7 +53,7 @@ export default function About() {
                     <h1 className='w-full font-1 desktop:text-5xl text-3xl desktop:mb-4 mb-2 font-normal text-qf-black'>
                         OUR STORY
                     </h1>
-                    <p className='desktop:w-9/10 text-qf-grey leading-8 font-light'>
+                    <p className={ bodyCls('desktop:w-9/10') }>
                         Lorem ipsum dolor sit amet, consectetur adipiscing
                         elit, sed do eiusmod tempor incididunt ut labore et dolore magna
                         aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
@@ -57,17 +67,19 @@ export default function About() {
                     <img src={ about } />
                 </div>
             </div>
-            <Profile 
-                name='CRISTIN Q. MURRAY'
-                bio='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
-                img={ cristin }
-            />
-            <Profile
-                name='BRANDT F. FINK'
-                bio='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
-                img={ brandt }
-            />
-            <div className='grid desktop:grid-cols-2 desktop:grid-rows-2 grid-rows-4 gap-y-8'>
+            <div className='flex flex-col gap-6'>
+                <Profile 
+                    name='CRISTIN Q. MURRAY'
+                    bio='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
+                    img={ cristin }
+                />
+                <Profile
+                    name='BRANDT F. FINK'
+                    bio='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
+                    img={ brandt }
+                />
+            </div>
+            <div className='grid desktop:grid-cols-2 desktop:grid-rows-2 grid-rows-4 gap-y-8 px-6'>
                 <DogProfile 
                     name='MOLSON' 
                     bio='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.' 
