639915a9d5fed4a5f726e5d8ad0cf72cff4c0b04
greydevv <greysonmurray.dev@gmail.com>
2022-07-10 11:54:52 -0400

    New lookups

---
+++ src/lookups/navigation.js
@@ -0,0 +1,26 @@
+export const navLinks = [ 
+    {
+        href: '/',
+        text: 'HOME',
+    },
+    {
+        href: '/about',
+        text: 'ABOUT',
+    },
+    {
+        href: '/hunt',
+        text: 'HUNT',
+    },
+    {
+        href: '/train',
+        text: 'TRAIN',
+    },
+    {
+        href: '/shop',
+        text: 'SHOP',
+    },
+    {
+        href: '/contact',
+        text: 'CONTACT',
+    },
+]
