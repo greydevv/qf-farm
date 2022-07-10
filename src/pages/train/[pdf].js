cbad2ee7c431a7b2feee8fb9fa8405c99a9a9f85
greydevv <greysonmurray.dev@gmail.com>
2022-07-10 18:14:44 -0400

    Temporarily use jpgs instead of pdfs on package page

--- src/pages/train/[pdf].js
+++ src/pages/train/[pdf].js
@@ -1,15 +1,16 @@
 import { useRouter } from 'next/router'
 import ArrowBackIcon from '@mui/icons-material/ArrowBack'
+import Image from 'next/image'
 import Link from 'next/link'
 
 export default function ViewPackage() {
     const router = useRouter()
     const { pdf } = router.query
 
-    const pdfUrl = process.env.NEXT_PUBLIC_S3_BUCKET_NAME + 'train/packages/' + pdf + '.pdf'
+    const pdfUrl = process.env.NEXT_PUBLIC_S3_BUCKET_NAME + 'train/packages/' + pdf + '.jpg'
     return (
-        <div className='w-full desktop:px-40 px-6'>
-            <div className='mt-6 mb-12'>
+        <div className='w-full desktop:pt-10 pt-6 desktop:px-40 px-6 desktop:pb-20 pb-5 flex flex-col desktop:gap-20 gap-10'>
+            <div className=''>
                 <Link href='/train'>
                     <div className='cursor-pointer flex gap-x-2 items-center'>
                         <ArrowBackIcon className='text-qf-orange' />
@@ -19,7 +20,10 @@ export default function ViewPackage() {
                     </div>
                 </Link>
             </div>
-            <iframe className='h-full w-full aspect-[8/11]' src={ pdfUrl } />
+            <div className='bg-qf-light-grey w-full relative mx-auto max-w-body'>
+                <img alt='Image loading...' src={ pdfUrl } className='w-full' />
+            </div>
+            {/* <iframe className='h-full w-full aspect-[8/11]' src={ pdfUrl } /> */}
         </div>
     )
 }
