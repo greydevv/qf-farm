a886d856e02ae6e2a04900d18f41c094914df49e
greydevv <greysonmurray.dev@gmail.com>
2022-07-10 18:27:27 -0400

    Style changes

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  images: {
-    domains: [
-        'cdn.quillfeatherfarm.com.s3-website-us-east-1.amazonaws.com'
-    ],
-  },
+    images: {
+        domains: [
+            'cdn.quillfeatherfarm.com.s3-website-us-east-1.amazonaws.com'
+        ],
+    },
 }
