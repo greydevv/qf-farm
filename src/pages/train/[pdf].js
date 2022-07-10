f0cf8ad33c78832695b03b9cb557bc7aa51a837b
greydevv <greysonmurray.dev@gmail.com>
2022-07-10 18:32:10 -0400

    Align the back arrow correctly

--- src/pages/train/[pdf].js
+++ src/pages/train/[pdf].js
@@ -10,7 +10,7 @@ export default function ViewPackage() {
     const pdfUrl = process.env.NEXT_PUBLIC_S3_BUCKET_NAME + 'train/packages/' + pdf + '.jpg'
     return (
         <div className='w-full desktop:pt-10 pt-6 desktop:px-40 px-6 desktop:pb-20 pb-5 flex flex-col desktop:gap-20 gap-10'>
-            <div className=''>
+            <div className='mx-auto flex max-w-body w-full'>
                 <Link href='/train'>
                     <div className='cursor-pointer flex gap-x-2 items-center'>
                         <ArrowBackIcon className='text-qf-orange' />
