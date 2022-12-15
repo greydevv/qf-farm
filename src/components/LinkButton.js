94479bf1d9bbf79ba94277e5d9074f6ced0b3e70
greydevv <greysonmurray.dev@gmail.com>
2022-12-14 22:24:52 -0500

    Add target prop

--- src/components/LinkButton.js
+++ src/components/LinkButton.js
@@ -2,10 +2,10 @@ import Link from 'next/link'
 import { composeCls } from 'components/shared'
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward'
 
-export default function LinkButton({ href, text, className }) {
+export default function LinkButton({ href, text, target, className }) {
     return (
         <div className='inline-block'>
-            <Link href={ href }>
+            <Link href={ href } target={ target }>
                 <div className={ composeCls(className, 'flex items-center gap-2 text-qf-white cursor-pointer rounded py-3 px-4') }>
                     <p className='text-qf-white'>
                         { text }
